a2e681452ed4f02d5d7118fad9bca50acff70872
7FM <41307817+7FM@users.noreply.github.com>
2021-06-07 16:12:31 +0200

    first steps for custom verilator integration

---
+++ tb/ariane_custom_tb_top.sv
@@ -0,0 +1,101 @@
+// Copyright 2018 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// Author: Florian Zaruba, ETH Zurich
+// Date: 19.03.2017
+// Description: Test-harness for Ariane
+//              Instantiates an AXI-Bus and memories
+
+module ariane_custom_tb_top #(
+    parameter int unsigned AXI_USER_WIDTH    = 1,
+    parameter int unsigned AXI_ADDRESS_WIDTH = 64,
+    parameter int unsigned AXI_DATA_WIDTH    = 64,
+    parameter int unsigned NUM_WORDS         = 2**25,         // memory size
+    parameter bit          StallRandomOutput = 1'b0,
+    parameter bit          StallRandomInput  = 1'b0
+) (
+    input  logic                           clk_i,
+    input  logic                           rtc_i,
+    input  logic                           rst_ni,
+    output logic [31:0]                    exit_o
+);
+
+    ariane_axi_soc::req_t    axi_ariane_req;
+    ariane_axi_soc::resp_t   axi_ariane_resp;
+
+    ariane_pkg::ariane_cfg_t ArianeCfg = ariane_pkg::ArianeDefaultConfig;
+
+    ariane#(
+        .ArianeCfg(ArianeCfg)
+    ) core(
+        .clk_i(clk_i),
+        .rst_ni(rst_ni),
+        // Core ID, Cluster ID and boot address are considered more or less static
+        .boot_addr_i(boot_addr_i),      // reset boot address
+        .hart_id_i(hart_id_i),          // hart id in a multicore environment (reflected in a CSR)
+        // Interrupt inputs
+        .irq_i(irq_i),                  // level sensitive IR lines, mip & sip (async)
+        .ipi_i(ipi_i),                  // inter-processor interrupts (async)
+        // Timer facilities
+        .time_irq_i(time_irq_i),        // timer interrupt in (async)
+        .debug_req_i(debug_req_i),      // debug request (async)
+
+        // memory side, AXI Master
+        .axi_req_o(axi_ariane_req),
+        .axi_resp_i(axi_ariane_resp)
+    );
+
+    localparam IdWidth   = 4;
+
+    AXI_BUS #(
+        .AXI_ADDR_WIDTH ( AXI_ADDRESS_WIDTH        ),
+        .AXI_DATA_WIDTH ( AXI_DATA_WIDTH           ),
+        .AXI_ID_WIDTH   ( IdWidth                  ),
+        .AXI_USER_WIDTH ( AXI_USER_WIDTH           )
+    ) dram_delayed();
+
+    axi_master_connect i_axi_master_connect_ariane (
+        .axi_req_i(axi_ariane_req),
+        .axi_resp_o(axi_ariane_resp),
+        .master(dram_delayed)
+    );
+
+    axi2mem #(
+        .AXI_ID_WIDTH   ( IdWidth ),
+        .AXI_ADDR_WIDTH ( AXI_ADDRESS_WIDTH        ),
+        .AXI_DATA_WIDTH ( AXI_DATA_WIDTH           ),
+        .AXI_USER_WIDTH ( AXI_USER_WIDTH           )
+    ) i_axi2mem (
+        .clk_i  ( clk_i        ),
+        .rst_ni ( ndmreset_n   ),
+        .slave  ( dram_delayed ),
+        .req_o  ( req          ),
+        .we_o   ( we           ),
+        .addr_o ( addr         ),
+        .be_o   ( be           ),
+        .data_o ( wdata        ),
+        .data_i ( rdata        )
+    );
+
+    sram #(
+        .DATA_WIDTH ( AXI_DATA_WIDTH ),
+        .NUM_WORDS  ( NUM_WORDS      )
+    ) i_sram (
+        .clk_i      ( clk_i                                                                       ),
+        .rst_ni     ( rst_ni                                                                      ),
+        .req_i      ( req                                                                         ),
+        .we_i       ( we                                                                          ),
+        .addr_i     ( addr[$clog2(NUM_WORDS)-1+$clog2(AXI_DATA_WIDTH/8):$clog2(AXI_DATA_WIDTH/8)] ),
+        .wdata_i    ( wdata                                                                       ),
+        .be_i       ( be                                                                          ),
+        .rdata_o    ( rdata                                                                       )
+    );
+
+endmodule
