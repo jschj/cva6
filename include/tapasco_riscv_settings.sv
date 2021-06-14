54b29aec1c00832fd75833ebd3008363f9655e37
7FM <41307817+7FM@users.noreply.github.com>
2021-06-14 17:32:38 +0200

    add file that will be used as collection for global includes

---
+++ include/tapasco_riscv_settings.sv
@@ -0,0 +1,11 @@
+`ifndef TAPASCO_RISCV_SETTINGS_SV
+`define TAPASCO_RISCV_SETTINGS_SV
+
+/*
+    File to set global defines which are required to use this core as PE for TaPaSCo.
+    A script is run to ensure that this file is included in EVERY other file.
+*/
+
+`define WT_DCACHE
+
+`endif
